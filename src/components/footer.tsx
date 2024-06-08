b3e673674db162d31f232c6514e1669dd9d5bb78
Franco Canzani <francocanzani@gmail.com>
2024-06-08 18:33:22 +0200

    adds opengraph and metadata

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className='my-6 w-full m-auto'>
-      <span className='text-sm font-medium text-center w-full'>
+    <footer className='my-6 w-full flex items-center justify-center'>
+      <span className='text-sm font-medium text-center w-full m-auto'>
         {'📞 +34 645 06 82 42'}
       </span>
     </footer>
